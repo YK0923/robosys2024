1771664f63dc2a93f2d59f2da7c6941384e0f8e5
Yamamoto Kurumi <s23c1144fj@s.chibakoudai.jp>
2024-10-31 12:22:01 +0900

    update

--- test.bash
+++ test.bash
@@ -1,14 +1,15 @@
 #!/bin/bash
-
+# SPDX-FileCopyrightText: 2024 Yamamoto Kurumi mail<s23c1144fj@s.chibakoudai.jp>
+# SPDX-License-Identifier: BSD-3-Clause
 ng () {
 	echo ${1}行目が違うよ	#$1はngの一番の引数
 	res=1	#追加
 }
 
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"	#LINENOとは行番号
-[ "$a" = 山田 ] || ng "$LINENO"	#ngに第一引数として$LINENOを付与
 
-exit "$res" #このシェルスクリプトの終了ステータスを返して終了
+out=$(seq 5 | ./PULS)
+[ "${out}" = 15 ] || ng "LINENO"
 
+[ "${res}" = 0 ] && echo OK 
+exit $res
